e64e98bd94d55d7369bc5b4f67c5586e842ada4d
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-09-06 00:35:47 -0400

    Updated block.json

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '45524d46103cc5b0f1a3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8fce0e31ad67c440e0ff');
